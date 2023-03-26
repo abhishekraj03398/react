be19c34416d274078deec368ecd27517ad3098dd
abhishekraj03398 <abhishekraj03398@gmail.com>
2023-03-26 21:54:19 +0530

    Its create a React Calculator App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MyCalculator from './components/MyCalculator';
+
+function App() {
+  return (
+    <div className="App">
+       <MyCalculator />
+    </div>
+  );
+}
+
+export default App;
